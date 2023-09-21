9f3c1a09eed275f5de8b4c2559df10e06c2b5cbc
dahuo98 <sxdahuo@gmail.com>
2023-09-21 00:05:37 -0400

    with flink dependency and message type

--- src/main/java/org/dahuo98/Main.java
+++ src/main/java/org/dahuo98/Main.java
@@ -1,19 +1,18 @@
 package org.dahuo98;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    private static final String INPUT_KAFKA_TOPIC = "sample-topic";
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    private static void createTopology(StreamExecutionEnvironment env) {
+
     }
+
+    public static void main(String[] args) {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    }
+
 }
